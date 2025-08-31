791adaca8305ce7d2a7ed24cdde577a1b1d3148a
Varunkumar23 <sarabuvarunkumar@gmail.com>
2025-08-31 12:32:02 +0530

    first commit

--- backend/models/Course.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },         
-  code: { type: String, required: true, unique: true }, 
-  description: { type: String },               
-});
-
-export default mongoose.model("Course", courseSchema);
